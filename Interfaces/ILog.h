56ab5478de0e863b8d855a22a5151277ce377697
MaxMelekesov <max.melekesov@gmail.com>
2025-02-13 13:59:35 +0300

    Create ILog.h

---
+++ Interfaces/ILog.h
@@ -0,0 +1,28 @@
+/**
+ * This file is part of m library.
+ *
+ * m library is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the LICENSE file in the
+ * project root for more information.
+ *
+ * Copyright (c) 2025 Max Melekesov <max.melekesov@gmail.com>
+ */
+
+#ifndef ILOG_H
+#define ILOG_H
+
+#include <cstdint>
+#include <string_view>
+
+namespace m::ifc {
+class ILog {
+ public:
+  using type = T;
+
+  virtual ~ILog() {}
+
+  virtual void add(std::string_view text) = 0;
+};
+}  // namespace m::ifc
+
+#endif  // ILOG_H

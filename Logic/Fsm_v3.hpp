ff6c302683da5f8b62078bee770360d2ef59905e
MaxMelekesov <max.melekesov@gmail.com>
2025-03-27 20:18:52 +0300

    Update Fsm_v3

--- Logic/Fsm_v3.hpp
+++ Logic/Fsm_v3.hpp
@@ -11,35 +11,31 @@
 #ifndef FSM_V3_H
 #define FSM_V3_H
 
-#include <array>
-#include <cstdint>
-#include <functional>
-#include <optional>
-
-namespace m {
-
+#include <tuple>
 #include <type_traits>
 #include <variant>
 
-template <typename Derived, typename... States>
+namespace m {
+
+template <typename Derived, typename EventTuple, typename... States>
 class Fsm_v3 {
  protected:
   using StateVariant = std::variant<States...>;
   StateVariant currentState;
 
+  // Список всех событий
+  using Events = EventTuple;
+
+ public:
+  constexpr Fsm_v3() = default;
+
+  // Установка состояния
   template <typename State>
   constexpr void setState() noexcept {
     currentState = State{};
   }
 
-  template <typename State>
-  [[nodiscard]] constexpr bool isCurrentState() const noexcept {
-    return std::holds_alternative<State>(currentState);
-  }
-
- public:
-  constexpr Fsm_v3() = default;
-
+  // Обработка события
   template <typename Event>
   constexpr void processEvent(const Event& event) {
     std::visit(
@@ -56,8 +52,24 @@ class Fsm_v3 {
         currentState);
   }
 
+  // Проверка всех событий для текущего состояния
+  constexpr void checkEvents() {
+    std::visit(
+        [&](auto&& state) {
+          std::apply(
+              [&](auto&&... events) {
+                (static_cast<Derived*>(this)->checkEvent(state, events), ...);
+              },
+              Events{});
+        },
+        currentState);
+  }
+
+  // Необработанные события по умолчанию
   template <typename State, typename Event>
-  constexpr void onUnhandledEvent(const State&, const Event&) {}
+  constexpr void onUnhandledEvent(const State&, const Event&) {
+    // std::cout << "Event not handled\n";
+  }
 };
 }  // namespace m
 

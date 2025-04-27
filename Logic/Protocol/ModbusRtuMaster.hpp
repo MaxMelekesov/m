15297c096819dedd511b2e0a5ce996908edde174
MaxMelekesov <max.melekesov@gmail.com>
2025-04-27 19:48:08 +0300

    Add ModbusRtuMaster and ModbusRtuMasterWrapper classes for Modbus protocol handling

---
+++ Logic/Protocol/ModbusRtuMaster.hpp
@@ -0,0 +1,355 @@
+/**
+ * This file is part of m library.
+ *
+ * m library is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the LICENSE file in the
+ * project root for more information.
+ *
+ * Copyright (c) 2025 Max Melekesov <max.melekesov@gmail.com>
+ */
+
+#ifndef MODBUS_RTU_MASTER_H
+#define MODBUS_RTU_MASTER_H
+
+#include <IIO_Async.hpp>
+#include <ITime.hpp>
+#include <TSerDes.hpp>
+#include <Timer.hpp>
+#include <Us.hpp>
+#include <cstdint>
+#include <optional>
+
+namespace m {
+
+template <CUs UsType>
+class ModbusRtuMaster {
+ public:
+  struct Timings {
+    UsType rx_delay;
+    UsType response_delay;
+  };
+
+  ModbusRtuMaster(m::ifc::IIO_Async& io, m::ifc::ITime<UsType>& time,
+                  Timings timings)
+      : io_(io),
+        time_(time),
+        timings_(timings),
+        tx_timer_(time),
+        start_rx_timer_(time),
+        rx_timer_(time) {}
+
+#pragma pack(push, 1)
+  struct Unit {
+   public:
+    Unit(uint8_t addr, uint16_t start_reg, uint16_t count)
+        : addr_(addr), start_reg_(start_reg), reg_count_(count) {}
+
+    Unit() {}
+
+   private:
+    uint8_t addr_{0u};
+    uint8_t cmd_{0u};
+    uint16_t start_reg_{0u};
+    uint16_t reg_count_{0u};
+    uint16_t crc_{0u};
+
+    friend class ModbusRtuMaster;
+  };
+#pragma pack(pop)
+  static_assert(sizeof(Unit) == 8, "Wrong sizeof(ModbusRtuMaster::Unit)");
+
+  uint16_t readMhrResponseSize(Unit& unit) { return unit.reg_count_ * 2u + 5u; }
+
+  bool readMhr(Unit& unit, std::span<uint8_t> response_buf) {
+    if (state_ != State::Idle) {
+      return false;
+    }
+    if (response_buf.size() != readMhrResponseSize(unit)) {
+      return false;
+    }
+
+    unit_ = unit;
+    response_buf_ = response_buf;
+
+    std::span<uint8_t> buf_tx{(uint8_t*)&unit_, sizeof(Unit)};
+    unit_.cmd_ = 0x03;
+    unit_.start_reg_ = (unit_.start_reg_ << 8) | (unit_.start_reg_ >> 8);
+    unit_.reg_count_ = (unit_.reg_count_ << 8) | (unit_.reg_count_ >> 8);
+    unit_.crc_ = crc16(std::span<uint8_t>{buf_tx.data(), buf_tx.size() - 2});
+    if (!io_.writeAsync(buf_tx)) {
+      return false;
+    }
+
+    tx_timer_.restart(
+        UsType{buf_tx.size() * 1'000'000 / io_.getBaudrate() + 1'000});
+
+    state_ = State::WaitTx;
+    response_ = std::nullopt;
+
+    return true;
+  }
+
+  uint32_t writeShrResponseSize() { return 8; }
+
+  bool writeShr(Unit& unit, std::span<uint8_t> response_buf) {
+    if (state_ != State::Idle) {
+      return false;
+    }
+    if (response_buf.size() != 8) {
+      return false;
+    }
+
+    unit_ = unit;
+    response_buf_ = response_buf;
+
+    std::span<uint8_t> buf_tx{(uint8_t*)&unit_, sizeof(Unit)};
+    unit_.cmd_ = 0x06;
+    unit_.start_reg_ = (unit_.start_reg_ << 8) | (unit_.start_reg_ >> 8);
+    unit_.reg_count_ = (unit_.reg_count_ << 8) | (unit_.reg_count_ >> 8);
+    unit_.crc_ = crc16(std::span<uint8_t>{buf_tx.data(), buf_tx.size() - 2});
+
+    if (!io_.writeAsync(buf_tx)) {
+      return false;
+    }
+
+    tx_timer_.restart(
+        UsType{buf_tx.size() * 1'000'000 / io_.getBaudrate() + 1'000});
+
+    state_ = State::WaitTx;
+    response_ = std::nullopt;
+
+    return true;
+  }
+
+  uint32_t writeMhrRequestSize(Unit& unit) { return unit.reg_count_ * 2u + 9u; }
+  uint32_t writeMhrResponseSize(Unit& unit) {
+    return unit.reg_count_ * 2u + 9u;
+  }
+
+  bool writeMhr(Unit& unit, std::span<uint8_t> data,
+                std::span<uint8_t> request_buf,
+                std::span<uint8_t> response_buf) {
+    if (state_ != State::Idle) {
+      return false;
+    }
+    if (data.size() != unit.reg_count_ * 2u) {
+      return false;
+    }
+    if (request_buf.size() != writeMhrRequestSize(unit)) {
+      return false;
+    }
+    if (response_buf.size() != writeMhrResponseSize(unit)) {
+      return false;
+    }
+
+    unit_ = unit;
+    response_buf_ = response_buf;
+    request_buf_ = request_buf;
+    unit_.cmd_ = 0x10;
+
+    auto offset = m::serialize(
+        request_buf, unit_.addr_, unit_.cmd_, uint8_t(unit_.start_reg_ >> 8),
+        uint8_t(unit_.start_reg_), uint8_t(unit_.reg_count_ >> 8),
+        uint8_t(unit_.reg_count_), uint8_t(unit_.reg_count_ * 2u));
+
+    std::copy(data.begin(), data.end(), request_buf.begin() + offset);
+    // Modbus send regs value in big endian
+    dataToBigEndian(
+        std::span<uint8_t>{request_buf.data() + offset, data.size()});
+
+    auto crc =
+        crc16(std::span<uint8_t>{request_buf.data(), request_buf.size() - 2});
+    request_buf[request_buf.size() - 2] = crc;
+    request_buf[request_buf.size() - 1] = crc >> 8;
+
+    if (!io_.writeAsync(request_buf)) {
+      return false;
+    }
+
+    tx_timer_.restart(
+        UsType{request_buf.size() * 1'000'000 / io_.getBaudrate() + 1'000});
+
+    state_ = State::WaitTx;
+    response_ = std::nullopt;
+
+    return true;
+  }
+
+  std::optional<std::span<uint8_t>> getResponse() { return response_; }
+
+  enum class Error : uint8_t {
+    None = 0,
+    IllegalFunction,
+    IllegalDataAddress,
+    IllegalDataValue,
+    SlaveDeviceFailure,
+    Acknowledge,
+    SlaveDeviceBusy,
+    NegativeAcknowledge,
+    MemParityError,
+    BadCrc,
+    BadCmd,
+    BadSize,
+    Corrupted,
+    WriteShrBadResponse
+  };
+  std::optional<Error> checkResponse(std::span<uint8_t> response) {
+    if (response.size() < 5) {
+      return Error::Corrupted;
+    }
+
+    auto crc = crc16(response.first(response.size() - 2));
+    auto [crc_origin] = m::deserialize<uint16_t>(response.last(2));
+    if (crc != crc_origin) {
+      return Error::BadCrc;
+    }
+
+    switch (response[1]) {
+      case 0x03:
+        if (response.size() != response[2] + 5u) {
+          return Error::BadSize;
+        }
+        break;
+      case 0x06: {
+        if (response.size() != 8u) return Error::BadSize;
+        std::span<uint8_t> temp{(uint8_t*)&unit_, sizeof(Unit)};
+        for (auto i = 0u; i < response.size(); ++i) {
+          if (response[i] != temp[i]) {
+            return Error::Corrupted;
+          }
+        }
+      } break;
+      case 0x10: {
+        if (response.size() != writeMhrResponseSize(unit_)) {
+          return Error::BadSize;
+        }
+        for (auto i = 0u; i < response.size(); ++i) {
+          if (response[i] != request_buf_[i]) {
+            return Error::Corrupted;
+          }
+        }
+      } break;
+      case 0x83:
+      case 0x86:
+      case 0x90:
+        if (response[2] >= uint8_t(Error::IllegalFunction) &&
+            response[2] <= uint8_t(Error::MemParityError)) {
+          return Error(response[2]);
+        }
+        break;
+
+      default:
+        return Error::BadCmd;
+        break;
+    }
+
+    return Error::None;
+  }
+
+  bool changeResponseEndian(std::span<uint8_t> response) {
+    if (response.size() >= 7) {
+      if (response[1] == 0x03) {
+        dataToBigEndian(response.subspan(3, response.size() - 5));
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void handle() {
+    switch (state_) {
+      case State::Idle:
+        break;
+      case State::WaitTx: {
+        if (io_.writeDone()) {
+          start_rx_timer_.restart(timings_.rx_delay);
+
+          state_ = State::StartRx;
+        } else {
+          if (tx_timer_.timeOver()) {
+            io_.abortWrite();
+            state_ = State::Idle;
+          }
+        }
+      } break;
+
+      case State::StartRx: {
+        if (start_rx_timer_.timeOver()) {
+          if (!io_.readAsync(response_buf_)) {
+            io_.abortRead();
+            state_ = State::Idle;
+          } else {
+            rx_timer_.restart(
+                UsType{response_buf_.size() * 1'000'000 / io_.getBaudrate() +
+                       500 + timings_.response_delay.value()});
+
+            state_ = State::WaitRx;
+          }
+        }
+      } break;
+
+      case State::WaitRx: {
+        if (io_.readDone()) {
+          response_ = response_buf_;
+          state_ = State::Idle;
+        } else {
+          if (rx_timer_.timeOver()) {
+            response_ =
+                std::span<uint8_t>{response_buf_.data(), io_.bytesAvailable()};
+            io_.abortRead();
+            state_ = State::Idle;
+          }
+        }
+      } break;
+
+      default:
+        break;
+    }
+  }
+
+  static bool dataToBigEndian(std::span<uint8_t> data) {
+    if (data.size() % 2 != 0) return false;
+    for (std::size_t i = 0; i < data.size() - 1; i += 2) {
+      std::swap(data[i], data[i + 1]);
+    }
+    return true;
+  }
+
+ private:
+  m::ifc::IIO_Async& io_;
+  m::ifc::ITime<UsType>& time_;
+  Timings timings_;
+  m::Timer<UsType> tx_timer_;
+  m::Timer<UsType> start_rx_timer_;
+  m::Timer<UsType> rx_timer_;
+
+  std::span<uint8_t> response_buf_;
+  std::optional<std::span<uint8_t>> response_;
+  std::span<uint8_t> request_buf_;
+
+  enum class State : uint8_t { Idle, WaitTx, StartRx, WaitRx };
+  State state_ = State::Idle;
+
+  Unit unit_;
+
+  uint16_t crc16(std::span<uint8_t> data) {
+    static const uint16_t table[2] = {0x0000, 0xA001};
+    uint16_t crc = 0xFFFF;
+    uint16_t xorv = 0;
+
+    for (auto i = 0u; i < data.size(); ++i) {
+      crc ^= data[i];
+
+      for (char bit = 0; bit < 8; bit++) {
+        xorv = crc & 0x01;
+        crc >>= 1;
+        crc ^= table[xorv];
+      }
+    }
+
+    return crc;
+  }
+};
+}  // namespace m
+
+#endif  // MODBUS_RTU_MASTER_H
---
+++ Logic/Protocol/ModbusRtuMasterWrapper.hpp
@@ -0,0 +1,108 @@
+/**
+ * This file is part of m library.
+ *
+ * m library is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the LICENSE file in the
+ * project root for more information.
+ *
+ * Copyright (c) 2025 Max Melekesov <max.melekesov@gmail.com>
+ */
+
+#ifndef MODBUS_RTU_MASTER_WRAPPER_H
+#define MODBUS_RTU_MASTER_WRAPPER_H
+
+#include <ModbusRtuMaster.hpp>
+
+namespace m {
+template <CUs UsType>
+class ModbusRtuMasterWrapper {
+ private:
+  using Unit = m::ModbusRtuMaster<UsType>::Unit;
+  using Error = m::ModbusRtuMaster<UsType>::Error;
+
+ public:
+  ModbusRtuMasterWrapper(m::ModbusRtuMaster<UsType>& modbus)
+      : modbus_(modbus) {}
+
+  bool readMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
+               std::span<uint8_t> data) {
+    Unit unit{addr, reg_addr, regs_num};
+    auto size = modbus_.readMhrResponseSize(unit);
+    auto span = std::span<uint8_t>{response_buf_.data(), size};
+    if (!modbus_.readMhr(unit, span)) {
+      return false;
+    }
+    auto resp = modbus_.getResponse();
+    while (!resp) {
+      modbus_.handle();
+      resp = modbus_.getResponse();
+    }
+
+    if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
+      return false;
+    } else {
+      if (!modbus_.changeResponseEndian(resp.value())) return false;
+      std::copy(resp.value().begin() + 3,
+                resp.value().begin() + 3 + regs_num * 2, data.begin());
+
+      return true;
+    }
+
+    return false;
+  }
+
+  bool writeShr(uint8_t addr, uint16_t reg_addr, uint16_t value) {
+    Unit unit{addr, reg_addr, value};
+    auto span = std::span<uint8_t>{request_buf_.data(), 8};
+    if (!modbus_.writeShr(unit, span)) {
+      return false;
+    }
+
+    auto resp = modbus_.getResponse();
+    while (!resp) {
+      modbus_.handle();
+      resp = modbus_.getResponse();
+    }
+
+    if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool writeMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
+                std::span<uint8_t> data) {
+    Unit unit{addr, reg_addr, regs_num};
+    auto request_size = modbus_.writeMhrRequestSize(unit);
+    auto request_span = std::span<uint8_t>{request_buf_.data(), request_size};
+
+    auto response_size = modbus_.writeMhrResponseSize(unit);
+    auto response_span =
+        std::span<uint8_t>{response_buf_.data(), response_size};
+
+    if (!modbus_.writeMhr(unit, data, request_span, response_span)) {
+      return false;
+    }
+    auto resp = modbus_.getResponse();
+    while (!resp) {
+      modbus_.handle();
+      resp = modbus_.getResponse();
+    }
+
+    if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
+      return false;
+    }
+
+    return true;
+  }
+
+ private:
+  m::ModbusRtuMaster<UsType>& modbus_;
+
+  std::array<uint8_t, 256> response_buf_;
+  std::array<uint8_t, 256> request_buf_;
+};
+}  // namespace m
+
+#endif  // MODBUS_RTU_MASTER_WRAPPER_H

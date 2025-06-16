ffd7746205d6829b2cf8ec91ec7760068b3636ff
MaxMelekesov <max.melekesov@gmail.com>
2025-06-16 13:55:43 +0300

    Update BitReg

--- Logic/Containers/BitReg.hpp
+++ Logic/Containers/BitReg.hpp
@@ -15,8 +15,7 @@
 #include <type_traits>
 
 /* Example usage:
- *
- * ```cpp
+
  *
  * // Example with DummyField for unused bits
  * struct ConfigField : public m::BitField<4, ConfigField, 15> {};
@@ -24,7 +23,7 @@
  *
  * // 32-bit register with unused bits
  * // Layout: Config(4) + Dummy(4) + Data(8) + Dummy(16) = 32 bits
- * using ControlRegister = m::Register<std::uint32_t,
+ * using ControlRegister = m::BitReg<std::uint32_t,
  *                                   ConfigField,       // Bits 0-3
  *                                   m::DummyField<4>,  // Bits 4-7 (unused)
  *                                   DataField,         // Bits 8-15
@@ -32,99 +31,65 @@
  *                                   >;
  *
  * ControlRegister ctrl;
- * ctrl.set<ConfigField>(10);  // Only tagged fields are accessible
- * ctrl.set<DataField>(255);   // Dummy fields are automatically skipped
- * ```
+ * ctrl.set<ConfigField>(10);
+ * ctrl.set<DataField>(255);
  */
 
 namespace m {
 
-template <typename T>
-concept CRegisterType = std::is_integral_v<T> && std::is_unsigned_v<T>;
-
 template <std::size_t Size, typename Derived, auto DefaultValue = 0>
   requires(Size > 0) && (Size <= 64)
 struct BitField {
-  using TagType = Derived;
   static constexpr std::size_t size = Size;
   static constexpr auto default_value = DefaultValue;
-  static constexpr auto max_value = (1ULL << Size) - 1;
+  static constexpr auto max_value = (1ULL << Size);
 
-  static_assert(static_cast<std::uint64_t>(DefaultValue) <= max_value,
+  static_assert(static_cast<std::uint64_t>(DefaultValue) < max_value,
                 "Default value exceeds bit field capacity");
 };
 
-template <typename T>
-concept CBitFieldTag =
-    std::is_base_of_v<BitField<T::size, T, T::default_value>, T>;
-
 template <std::size_t Size>
   requires(Size > 0) && (Size <= 64)
-struct DummyField {
-  struct DummyTag {};
-  using TagType = DummyTag;
-  static constexpr std::size_t size = Size;
-  static constexpr int default_value = 0;
-};
+struct DummyField : public BitField<Size, DummyField<Size>, 0> {};
 
-namespace {
+template <typename T>
+concept CBitField = requires {
+  T::size;
+  T::default_value;
+  T::max_value;
+} && std::is_base_of_v<BitField<T::size, T, T::default_value>, T>;
 
-template <typename Tag, typename... Fields>
-concept CIsRegisterTag = (std::is_same_v<Tag, typename Fields::TagType> || ...);
+template <typename Field, typename... Fields>
+concept CField = (std::is_same_v<Field, Fields> || ...);
 
-template <typename Tag, typename FirstField, typename... RestFields>
-constexpr auto getFieldByTag() {
-  if constexpr (std::is_same_v<Tag, typename FirstField::TagType>) {
-    return FirstField{};
-  } else if constexpr (sizeof...(RestFields) > 0) {
-    return getFieldByTag<Tag, RestFields...>();
-  }
-}
+template <typename T>
+concept CRegStorage = std::is_integral_v<T> && std::is_unsigned_v<T>;
 
-template <typename Tag, typename FirstField, typename... RestFields>
-constexpr std::size_t calculateOffsetByTag() {
-  if constexpr (std::is_same_v<Tag, typename FirstField::TagType>) {
-    return 0;
-  } else if constexpr (sizeof...(RestFields) > 0) {
-    return FirstField::size + calculateOffsetByTag<Tag, RestFields...>();
-  } else {
-    return 0;
-  }
-}
-}  // namespace
-
-template <CRegisterType StorageType, typename... Fields>
+template <CRegStorage StorageType, CBitField... Fields>
   requires(sizeof...(Fields) > 0)
-class Register {
+class BitReg {
  public:
-  constexpr Register() : data_(0) { ((setFieldDefault<Fields>()), ...); }
+  constexpr BitReg() : data_(0) { ((setFieldDefault<Fields>()), ...); }
 
-  explicit constexpr Register(StorageType initial_value)
-      : data_(initial_value) {}
+  explicit constexpr BitReg(StorageType initial_value) : data_(initial_value) {}
 
-  template <typename Tag>
-    requires CIsRegisterTag<Tag, Fields...>
+  template <CBitField Field>
+    requires CField<Field, Fields...>
   constexpr auto get() const {
-    constexpr std::size_t offset = getOffset<Tag>();
-    constexpr std::size_t size = getSize<Tag>();
-    constexpr StorageType field_mask = (StorageType(1) << size) - 1;
+    constexpr auto field_info = getFieldInfo<Field, Fields...>();
 
-    return static_cast<decltype(getFieldByTag<Tag, Fields...>().default_value)>(
-        (data_ >> offset) & field_mask);
+    return static_cast<decltype(field_info.default_value)>(
+        (data_ >> field_info.offset) & field_info.field_mask);
   }
 
-  template <typename Tag, typename Value>
-    requires CIsRegisterTag<Tag, Fields...>
+  template <CBitField Field, typename Value>
+    requires CField<Field, Fields...>
   constexpr void set(Value value) {
-    constexpr std::size_t offset = getOffset<Tag>();
-    constexpr std::size_t size = getSize<Tag>();
-    constexpr StorageType field_mask = (StorageType(1) << size) - 1;
-    constexpr StorageType register_mask = getMask<Tag>();
+    constexpr auto field_info = getFieldInfo<Field, Fields...>();
+    const StorageType masked_value = static_cast<StorageType>(value);
 
-    const StorageType masked_value =
-        static_cast<StorageType>(value) & field_mask;
-
-    data_ = (data_ & ~register_mask) | (masked_value << offset);
+    data_ = (data_ & ~field_info.register_mask) |
+            (masked_value << field_info.offset);
   }
 
   constexpr StorageType raw() const { return data_; }
@@ -143,33 +108,31 @@ class Register {
   static_assert(total_size <= sizeof(StorageType) * 8,
                 "Total field size exceeds storage type capacity");
 
-  template <typename Tag>
-    requires CIsRegisterTag<Tag, Fields...>
-  static constexpr std::size_t getOffset() {
-    return calculateOffsetByTag<Tag, Fields...>();
+  template <std::size_t Offset, std::size_t Size, auto DefaultValue>
+  struct FieldInfo {
+    static constexpr std::size_t offset = Offset;
+    static constexpr std::size_t size = Size;
+    static constexpr auto default_value = DefaultValue;
+    static constexpr StorageType field_mask = (StorageType(1) << Size) - 1;
+    static constexpr StorageType register_mask = field_mask << Offset;
+  };
+
+  template <CBitField Field, CBitField FirstField, CBitField... RestFields>
+  static constexpr auto getFieldInfo() {
+    if constexpr (std::is_same_v<Field, FirstField>) {
+      return FieldInfo<0, FirstField::size, FirstField::default_value>{};
+    } else if constexpr (sizeof...(RestFields) > 0) {
+      auto next_info = getFieldInfo<Field, RestFields...>();
+      return FieldInfo<FirstField::size + next_info.offset, next_info.size,
+                       next_info.default_value>{};
+    }
   }
 
-  template <typename Tag>
-    requires CIsRegisterTag<Tag, Fields...>
-  static constexpr std::size_t getSize() {
-    return decltype(getFieldByTag<Tag, Fields...>())::size;
-  }
-
-  template <typename Tag>
-    requires CIsRegisterTag<Tag, Fields...>
-  static constexpr StorageType getMask() {
-    constexpr std::size_t size = getSize<Tag>();
-    constexpr std::size_t offset = getOffset<Tag>();
-    constexpr StorageType field_mask = (StorageType(1) << size) - 1;
-    return field_mask << offset;
-  }
-
-  template <typename Field>
+  template <CBitField Field>
   constexpr void setFieldDefault() {
-    if constexpr (!std::is_same_v<typename Field::TagType,
-                                  typename DummyField<Field::size>::DummyTag>) {
+    if constexpr (!std::is_same_v<Field, DummyField<Field::size>>) {
       if constexpr (Field::default_value != 0) {
-        set<typename Field::TagType>(Field::default_value);
+        set<Field>(Field::default_value);
       }
     }
   }

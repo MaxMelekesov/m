209f86ed6e4ede6c4bfdfa91a7dd99327834c5e3
MaxMelekesov <max.melekesov@gmail.com>
2025-10-08 16:39:23 +0300

    Remove FlashIC and IcSync headers; add IoSyncAdapter implementation

---
+++ Interfaces/IoSyncAdpter.hpp
@@ -0,0 +1,62 @@
+/**
+ * This file is part of m library.
+ *
+ * m library is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the LICENSE file in the
+ * project root for more information.
+ *
+ * Copyright (c) 2025 Max Melekesov <max.melekesov@gmail.com>
+ */
+
+#ifndef IIOSYNCADAPTER_HPP
+#define IIOSYNCADAPTER_HPP
+
+#include <Bps.hpp>
+#include <IIO_Async.hpp>
+#include <IIO_Sync.hpp>
+#include <ITime.hpp>
+#include <Timeout.hpp>
+#include <functional>
+
+namespace m::ifc {
+
+template <CBps Baudrate, typename TimeUnitT, m::ifc::CTime<TimeUnitT> TimeT>
+class IoSyncAdapter : public IIO_Sync<Baudrate> {
+ public:
+  IoSyncAdapter(
+      IIO_Async<Baudrate>& io, TimeT& time,
+      std::function<TimeUnitT(Baudrate baud, std::size_t size)>&& calc_timeout)
+      : io_(io), time_(time), calc_timeout_(std::move(calc_timeout)) {}
+
+  bool write(std::span<uint8_t const> data) override {
+    if (!io_.writeAsync(data)) return false;
+    return m::execWithTimeout(
+        time_, [&]() { return io_.writeDone(); },
+        calc_timeout_(io_.getBaudrate(), data.size()));
+  }
+
+  bool read(std::span<uint8_t> data) override {
+    if (!io_.readAsync(data)) return false;
+    return m::execWithTimeout(
+        time_, [&]() { return io_.readDone(); },
+        calc_timeout_(io_.getBaudrate(), data.size()));
+  }
+
+  Baudrate getBaudrate() override { return io_.getBaudrate(); }
+  bool setBaudrate(Baudrate baud) override { return io_.setBaudrate(baud); }
+
+  bool error() override { return io_.error(); }
+
+ private:
+  IIO_Async<Baudrate>& io_;
+  TimeT& time_;
+  std::function<TimeUnitT(Baudrate baud, std::size_t size)>&& calc_timeout_;
+};
+
+static_assert(
+    CIO_Sync<IoSyncAdapter<Bps<uint32_t>, uint32_t, m::ifc::ITime<uint32_t>>>,
+    "IIO_Sync must satisfy CIO_Sync concept");
+
+}  // namespace m::ifc
+
+#endif  // IIOSYNCADAPTER_HPP

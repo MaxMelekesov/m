a5df4601a62a470ba4464c983a27df309d9ff54c
MaxMelekesov <max.melekesov@gmail.com>
2026-01-31 13:14:29 +0300

    Refactor ModbusRtuMasterWrapper methods to use coroutines

--- Logic/Protocol/ModbusRtuMasterWrapper.hpp
+++ Logic/Protocol/ModbusRtuMasterWrapper.hpp
@@ -11,6 +11,8 @@
 #ifndef MODBUS_RTU_MASTER_WRAPPER_HPP
 #define MODBUS_RTU_MASTER_WRAPPER_HPP
 
+#include <CoroMutex.hpp>
+#include <CoroScheduler.hpp>
 #include <ModbusRtuMaster.hpp>
 
 namespace m {
@@ -23,55 +25,61 @@ class ModbusRtuMasterWrapper {
  public:
   ModbusRtuMasterWrapper(Mdbs& modbus) : modbus_(modbus) {}
 
-  bool readMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
-               std::span<uint8_t> data) {
+  auto readMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
+               std::span<uint8_t> data) -> Task<bool> {
+    co_await mutex_.lock();
     Unit unit{addr, reg_addr, regs_num};
     auto size = modbus_.readMhrResponseSize(unit);
     auto span = std::span<uint8_t>{response_buf_.data(), size};
     if (!modbus_.readMhr(unit, span)) {
-      return false;
+      co_return false;
     }
     auto resp = modbus_.getResponse();
     while (!resp) {
       modbus_.handle();
       resp = modbus_.getResponse();
+      co_await CoroScheduler::yield();
     }
 
     if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
-      return false;
+      co_return false;
     } else {
-      if (!modbus_.changeResponseEndian(resp.value())) return false;
+      if (!modbus_.changeResponseEndian(resp.value())) co_return false;
       std::copy(resp.value().begin() + 3,
                 resp.value().begin() + 3 + regs_num * 2, data.begin());
 
-      return true;
+      co_return true;
     }
 
-    return false;
+    co_return false;
   }
 
-  bool writeShr(uint8_t addr, uint16_t reg_addr, uint16_t value) {
+  auto writeShr(uint8_t addr, uint16_t reg_addr, uint16_t value)
+      -> m::Task<bool> {
+    co_await mutex_.lock();
     Unit unit{addr, reg_addr, value};
     auto span = std::span<uint8_t>{request_buf_.data(), 8};
     if (!modbus_.writeShr(unit, span)) {
-      return false;
+      co_return false;
     }
 
     auto resp = modbus_.getResponse();
     while (!resp) {
       modbus_.handle();
       resp = modbus_.getResponse();
+      co_await CoroScheduler::yield();
     }
 
     if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
-      return false;
+      co_return false;
     }
 
-    return true;
+    co_return true;
   }
 
-  bool writeMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
-                std::span<uint8_t> data) {
+  auto writeMhr(uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
+                std::span<uint8_t> data) -> m::Task<bool> {
+    co_await mutex_.lock();
     Unit unit{addr, reg_addr, regs_num};
     auto request_size = modbus_.writeMhrRequestSize(unit);
     auto request_span = std::span<uint8_t>{request_buf_.data(), request_size};
@@ -81,37 +89,44 @@ class ModbusRtuMasterWrapper {
         std::span<uint8_t>{response_buf_.data(), response_size};
 
     if (!modbus_.writeMhr(unit, data, request_span, response_span)) {
-      return false;
+      co_return false;
     }
     auto resp = modbus_.getResponse();
     while (!resp) {
       modbus_.handle();
       resp = modbus_.getResponse();
+      co_await CoroScheduler::yield();
     }
 
     if (auto err = modbus_.checkResponse(resp.value()); err != Error::None) {
-      return false;
+      co_return false;
     }
 
-    return true;
+    co_return true;
   }
 
  private:
   Mdbs& modbus_;
+  m::CoroMutex mutex_;
 
   std::array<uint8_t, 256> response_buf_;
   std::array<uint8_t, 256> request_buf_;
 };
 
 template <typename T>
+
 concept CModbusRtuMasterWrapper =
     requires(T wrapper, uint8_t addr, uint16_t reg_addr, uint16_t regs_num,
              uint16_t value, std::span<uint8_t> data) {
-      { wrapper.readMhr(addr, reg_addr, regs_num, data) } -> std::same_as<bool>;
-      { wrapper.writeShr(addr, reg_addr, value) } -> std::same_as<bool>;
+      {
+        wrapper.readMhr(addr, reg_addr, regs_num, data)
+      } -> std::same_as<m::Task<bool>>;
+      {
+        wrapper.writeShr(addr, reg_addr, value)
+      } -> std::same_as<m::Task<bool>>;
       {
         wrapper.writeMhr(addr, reg_addr, regs_num, data)
-      } -> std::same_as<bool>;
+      } -> std::same_as<m::Task<bool>>;
     };
 
 static_assert(

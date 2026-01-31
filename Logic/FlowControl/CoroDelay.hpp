13386cc6f52192a4c4994b5cedbc82f4e2dc0cb7
MaxMelekesov <max.melekesov@gmail.com>
2026-01-31 13:14:19 +0300

    Add CoroDelay

---
+++ Logic/FlowControl/CoroDelay.hpp
@@ -0,0 +1,30 @@
+/**
+ * This file is part of m library.
+ *
+ * m library is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. See the LICENSE file in the
+ * project root for more information.
+ *
+ * Copyright (c) 2026 Max Melekesov <max.melekesov@gmail.com>
+ */
+
+#ifndef CORO_DELAY_HPP
+#define CORO_DELAY_HPP
+
+#include <CoroScheduler.hpp>
+#include <ITime.hpp>
+
+namespace m {
+
+template <m::ifc::CTime TimeT>
+inline auto coroDelay(TimeT& time,
+                      decltype(std::declval<TimeT&>().getTick()) delay)
+    -> m::Task<void> {
+  auto start = time.getTick();
+  co_await m::CoroScheduler::until(
+      [&]() { return time.getDiff(start) >= delay; });
+}
+
+}  // namespace m
+
+#endif  // CORO_DELAY_HPP
